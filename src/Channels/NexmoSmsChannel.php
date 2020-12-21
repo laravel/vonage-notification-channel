beb576d6148798b388385819a70bd1a2ec38e760
Braunson Yager <Braunson@users.noreply.github.com>
2020-12-21 09:20:03 -0500

    Update NexmoSmsChannel.php

--- src/Channels/NexmoSmsChannel.php
+++ src/Channels/NexmoSmsChannel.php
@@ -63,7 +63,7 @@ class NexmoSmsChannel
         ];
 
         if ($message->statusCallback) {
-            $payload['client_ref'] = $message->clientReference;
+            $payload['client-ref'] = $message->clientReference;
         }
 
         return ($message->client ?? $this->nexmo)->message()->send($payload);
